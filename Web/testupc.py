a66dbcb7bc94962299afcf01bd8f59531fd9bd24
Alex Roth <me@magmastone.net>
2015-04-14 09:44:19 -0400

    Added checkin/out to web server

--- Web/testupc.py
+++ Web/testupc.py
@@ -2,7 +2,8 @@ import web
 import json
 
 urls= (
-	'/', 'index'
+	'/', 'index',
+	'/checkin','checkin'
 )
 
 
@@ -24,6 +25,29 @@ class index:
 			return json.dumps({'status':"G"})
 		except:
 			return json.dumps({'status':"N"})
+class checkin:
+	def GET(self):
+		i=web.input()
+		todos= db.select('frg', where="upcid='"+i.upc+"'");
+		try:
+			db.query("UPDATE `frg` SET `quantity`=`quantity`-1,`name`='"+i["uname"]+"' WHERE `upcid`="+i["upc"]+" AND `quantity`>0")
+			frg= db.select('frg', where="upcid='"+i.upc+"'");
+			return json.dumps(dict({'status':"G"}.items()+frg[0].items()))
+		except:
+			return json.dumps({'status':"N", 'upc':i.upc})
+	def POST(self):
+		i=web.input()
+		#try:
+		quant=1
+		try:
+			quant=int(i["quant"])
+		except:
+			pass
+		db.query("INSERT OR IGNORE INTO frg (upcid,name,expiry,quantity) VALUES ('"+i["upc"]+"','"+i["uname"]+"','"+i["expiry"]+"',0)")
+		db.query("UPDATE `frg` SET `quantity`=`quantity`+"+str(quant)+",`name`='"+i["uname"]+"',`expiry`='"+i["expiry"]+"' WHERE `upcid`="+i.upc)
+		frg= db.select('frg', where="upcid='"+i.upc+"'");
+		return json.dumps(dict({'status':"G"}.items()+frg[0].items()))
+
 if __name__ == "__main__": 
     app = web.application(urls, globals())
     app.run()
